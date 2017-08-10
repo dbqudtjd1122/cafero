92274095bf4c1ec17b94aa8a8aba7cad70af0690
cjswldks <ejrdnr123@gmail.com>
2017-08-10 16:58:19 +0900

    sadfdf

--- TeamProject/userapp/src/main/java/com/cafe/userapp/tab_frgment1.java
+++ TeamProject/userapp/src/main/java/com/cafe/userapp/tab_frgment1.java
@@ -22,7 +22,9 @@ import java.util.List;
 public class tab_frgment1 extends Fragment {
 
     private View view = null;
+    private boolean calling = false;
 
+    AdapterCustom adapter = null;
 
     public tab_frgment1() {
     }
@@ -32,8 +34,96 @@ public class tab_frgment1 extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.tab_frgment1, container, false);
 
+        GridView lv = (GridView) view.findViewById(R.id.gridView);
+
+        // 데이터 생성
+        List<ModelItem> list = new ArrayList<>();
+
+        // 어댑터 객체 생성
+        adapter = new AdapterCustom(getActivity(), R.layout.list_item, R.id.dataItem01, list);
+        adapter.setInitData(0, 5);
+
+        // 리스뷰와 어댑터 연결
+        lv.setAdapter(adapter);
+
+        // item click listener
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                ModelItem s = (ModelItem) parent.getItemAtPosition(position);
+//                Toast.makeText(getActivity(), s.getDataItem01() + " clicked", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        // item long click listener
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView parent, View v, int posotion, long id) {
+                return true;
+            }
+        });
+
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+                // Sample calculation to determine if the last item is fully visible.
+                final int lastItem = firstVisibleItem + visibleItemCount;
+                if (lastItem == totalItemCount) {
+                    if (!calling) {
+                        calling = true;
+                        //to avoid multiple calls for last item
+                        new HttpLogin().execute(adapter.getCount(), adapter.getCount() + 10);
+                    }
+                }
+            }
+        });
+
+
         return view;
 
+    }
+
+    public class HttpLogin extends AsyncTask<Integer, Integer, List<ModelItem>> {
+        private ProgressDialog waitDlg = null;
+
+//        @Override
+//        protected void onPreExecute() {
+//            super.onPreExecute();
+//
+//            //서버에 요청 동안 Wating dialog를 보여주도록 한다.
+//            waitDlg = new ProgressDialog(getActivity());
+//            waitDlg.setMessage(" 버전 확인 중");
+//            waitDlg.show();
+//        }
+
+        @Override
+        protected List<ModelItem> doInBackground(Integer... params) {
+
+            List<ModelItem> list = adapter.makeData(params[0], params[1]);
+            return list;
+        }
+
+        @Override
+        protected void onPostExecute(List<ModelItem> result) {
+            super.onPostExecute(result);
+
+            //서버 요청 완료 후 Waiting dialog를 제거한다.
+            if (waitDlg != null) {
+                waitDlg.dismiss();
+                waitDlg = null;
+            }
+
+            calling = false;
+
+            adapter.addAll(result);
+        }
     }
 
     @Override
